2bd0bed413fc7a731a05fd29ac8e4a9ad7173704
gyan2003 <gyanendarsingh891@gmail.com>
2024-08-05 00:29:57 +0530

    client updated

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -15,7 +15,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://localhost:3001/assets/${image}`}
+        src={`https://instabase-1.onrender.com/assets/${image}`}
       />
     </Box>
   );
